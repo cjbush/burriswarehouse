47466f6253b62115e2e90eb5f2eef09bf1f75caf
calebmays85@gmail.com <calebmays85@gmail.com@e2e658d7-dff6-02fa-4d1e-a381fe90bd90>
2011-01-21 20:40:38 +0000

    Npc class modified further. Now extends AnimatedModel, however, the information about loading (filename, foldername, etc) is where I'm going to get a bit stuck here. Need Dan to put up a model and just get the stuff placed in the warehouse...at least, that's what I think needs to happen.
    Anyway, it should work, but nothing really happens yet. Shouldn't have any errors.
    
    CM

--- VirtualWarehouse/src/code/npc/logic/Npc.java
+++ VirtualWarehouse/src/code/npc/logic/Npc.java
@@ -2,15 +2,32 @@ package code.npc.logic;
 
 import java.util.LinkedList;
 
+import com.jme.math.Vector3f;
 
-public class Npc {
+import code.model.AnimatedModel;
+import code.model.player.RandomPerson;
+
+
+public class Npc extends AnimatedModel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2393676632933569642L;
+
 	private LinkedList<Coordinate> path;
+	
+	// this beginning and ending coordinate values are in grid-space; NOT real space. All values will be
+	// ints, and not floats, at least for assignment, because for the translation, we're just going
+	// go get the grid node at begX, begY, and translate it on the fly.
 	private int beginningX;
 	private int beginningY;
 	private int endingX;
 	private int endingY;
 	
-	public Npc(int begX, int begY, int endX, int endY){
+	public Npc(int begX, int begY, int endX, int endY, String filePath, String folderPath, String defaultAnimName,
+			String animFilePath, int repeatType, Vector3f forwardVector, Vector3f rightVector, Vector3f upVector,
+			RandomPerson randomPerson){
+		super(filePath, folderPath, defaultAnimName, animFilePath, repeatType, forwardVector, rightVector, DEFAULT_UP, null);
 		this.beginningX = begX;
 		this.beginningY = begY;
 		this.endingX = endX;
@@ -30,6 +47,88 @@ public class Npc {
 		//return path;
 	}
 	
-	
+	public void move(){
+		// something like this. then get the location, and move the thing.
+		// if the path is out of moves to do, then we're going to have to create another new
+		// beginning and ending X,Y to be able to make another path to go.
+		//path.remove();
+	}
 
+	@Override
+	public void rotateXNegAnim() {
+		
+		
+	}
+
+	@Override
+	public void rotateXPosAnim() {
+		
+		
+	}
+
+	@Override
+	public void rotateYNegAnim() {
+		
+		
+	}
+
+	@Override
+	public void rotateYPosAnim() {
+		
+		
+	}
+
+	@Override
+	public void rotateZNegAnim() {
+		
+		
+	}
+
+	@Override
+	public void rotateZPosAnim() {
+		
+		
+	}
+
+	@Override
+	public void stationaryAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateBackwardAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateDownAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateForwardAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateLeftAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateRightAnim() {
+		
+		
+	}
+
+	@Override
+	public void translateUpAnim() {
+		
+		
+	}
 }
