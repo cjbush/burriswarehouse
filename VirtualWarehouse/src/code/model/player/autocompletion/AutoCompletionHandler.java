2320e5295a7cc5a0e84a3ebd1cbce6244bacc6bf
calebmays85@gmail.com <calebmays85@gmail.com@e2e658d7-dff6-02fa-4d1e-a381fe90bd90>
2011-03-04 00:34:39 +0000

    This revision is specifically to try to help fix some of the rotation things that happen when the autocompletion gets close to a corner. I tried making the rotation happen gradually, but that turned out to be pretty complicated, so I actually made the rotation snappier (the jack DOESN'T do any rotating if it's too close to the corner.)
    I also did this for the players in the Npc class. That way, they look a bit snappier too. -CM

--- VirtualWarehouse/src/code/model/player/autocompletion/AutoCompletionHandler.java
+++ VirtualWarehouse/src/code/model/player/autocompletion/AutoCompletionHandler.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.jme.math.FastMath;
+import com.jme.math.Matrix3f;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 
@@ -12,7 +13,7 @@ import code.model.player.Player;
 import code.util.Coordinate;
 import code.util.DatabaseHandler;
 
-public class AutoCompletionHandler{
+public class AutoCompletionHandler {
 	private ArrayList<Waypoint> path;
 	private DatabaseHandler db;
 	private ResultSet rs;
@@ -23,9 +24,11 @@ public class AutoCompletionHandler{
 	private boolean active;
 	private boolean walking;
 	private int section;
-	
+	private float playerDirection;
+	protected static final Matrix3f incr = new Matrix3f();
+
 	private boolean enabled = true;
-	
+
 	public static final int FORWARD = 0;
 	public static final int ROTLEFT = 1;
 	public static final int ROTRIGHT = 2;
@@ -34,175 +37,196 @@ public class AutoCompletionHandler{
 	public static final int PICKUPBOX = 5;
 	public static final int PUTDOWNBOX = 6;
 	public static final int PICKUPPALLET = 7;
-	
-	public AutoCompletionHandler(Player player, int pickjob, int start, int finish){
-		if(!enabled) return;
-		try{
-			db = new DatabaseHandler("joseph.cedarville.edu", "vwburr", "warehouse", "vwburr15");
-		}
-		catch(Exception e){
+
+	public AutoCompletionHandler(Player player, int pickjob, int start,
+			int finish) {
+		if (!enabled)
+			return;
+		try {
+			db = new DatabaseHandler("joseph.cedarville.edu", "vwburr",
+					"warehouse", "vwburr15");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		//this.counter = path.indexOf(this.start);
+
+		// this.counter = path.indexOf(this.start);
 		this.counter = 0;
 		this.player = player;
 		this.walking = false;
 		this.active = false;
 		this.section = 1;
 	}
-	
-	public boolean isActive(){return this.active;}
-	public void activate(){
-		if(!enabled) return;
-		
+
+	public boolean isActive() {
+		return this.active;
+	}
+
+	public void activate() {
+		if (!enabled)
+			return;
+
 		player.setCollisionDetection(false);
-		
-		try{
+
+		try {
 			this.path = new ArrayList<Waypoint>();
-			rs = db.executeQuery("SELECT * FROM PICKWAYPOINT WHERE pickjob = 1 AND section="+section);
-		
-		
-			while(rs.next()){
-				Waypoint w = new Waypoint(rs.getFloat(3), rs.getFloat(4), rs.getInt(2), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getInt(8));
+			rs = db
+					.executeQuery("SELECT * FROM PICKWAYPOINT WHERE pickjob = 1 AND section="
+							+ section);
+
+			while (rs.next()) {
+				Waypoint w = new Waypoint(rs.getFloat(3), rs.getFloat(4), rs
+						.getInt(2), rs.getString(5), rs.getInt(6),
+						rs.getInt(7), rs.getInt(8));
 				path.add(w);
 			}
-			
-			if(path.size() == 0){
+
+			if (path.size() == 0) {
 				section = -1;
 				deactivate();
 			}
-			
+
 			this.start = path.get(0);
-			this.finish = path.get(path.size()-1);
-			
+			this.finish = path.get(path.size() - 1);
+
 			this.active = true;
-			if(!player.inVehicle())
+			if (!player.inVehicle()) {
 				player.setLocalTranslation(start.getX(), .1f, start.getZ());
-			else player.getVehicleBeingUsed().setLocalTranslation(start.getX(), .1f, start.getZ());
+			}
+
+			else {
+				// I think instead of doing this, you should take either take
+				// the player off the vehicle, reset player and vehicle,
+				// and start from the beginning, or you should just start from
+				// where the player got on the vehicle... -CM
+				player.getVehicleBeingUsed().setLocalTranslation(start.getX(),
+						.1f, start.getZ());
+			}
 			this.counter++;
 			return;
-		}
-		catch(SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void deactivate(){
+
+	public void deactivate() {
 		player.setCollisionDetection(true);
 		this.active = false;
 		advance();
 		return;
 	}
-	
-	public void advance(){
+
+	public void advance() {
 		counter = 0;
 		section++;
 	}
-	
-	public void update(){
-		if(!enabled) return;
-		if(!active) return;
-		
+
+	public void update() {
+		if (!enabled)
+			return;
+		if (!active)
+			return;
+
 		player.getDebugHud().setAutoCount(counter);
-		
+
 		Waypoint w = path.get(this.counter);
-		
+
 		int action = w.getAction();
-		
+
 		float x = w.getX();
 		float z = w.getZ();
-		
+
 		float myX, myZ;
-		
-		if(action == GETONPJ){
+
+		if (action == GETONPJ) {
 			player.checkVehicleEnterExit(true);
 			counter++;
-		}
-		else if(action == GETOFFPJ){
+		} else if (action == GETOFFPJ) {
 			player.checkVehicleEnterExit(true);
 			counter++;
-		}
-		else if(action == PICKUPBOX){
+		} else if (action == PICKUPBOX) {
 			player.checkProductGetDrop(true);
 			counter++;
-		}
-		else if(action == PUTDOWNBOX){
+		} else if (action == PUTDOWNBOX) {
 			player.checkProductGetDrop(true);
 			counter++;
-		}
-		else if(action == PICKUPPALLET){
+		} else if (action == PICKUPPALLET) {
 			player.getVehicleBeingUsed().checkForPalletPickup(true);
 			counter++;
-		}
-		else{
-		
-			if(!player.inVehicle()){
+		} else {
+
+			if (!player.inVehicle()) {
 				myX = player.getLocalTranslation().getX();
 				myZ = player.getLocalTranslation().getZ();
-			}
-			else{
+				if ((Math.abs(myX - x) > .25f) || (Math.abs(myZ - z) > .25f)) {
+					playerDirection = -(FastMath.atan2(myZ - z, myX - x));
+					player.setLocalRotation(new Quaternion().fromAngleAxis(
+						playerDirection, Vector3f.UNIT_Y));
+				}
+			} else {
 				myX = player.getVehicleBeingUsed().getLocalTranslation().getX();
 				myZ = player.getVehicleBeingUsed().getLocalTranslation().getZ();
+				// if i'm far enough to care about direction...otherwise the 
+				// corners look a little funky...
+				if ((Math.abs(myX - x) > .75f) || (Math.abs(myZ - z) > .75f)) {
+					playerDirection = -(FastMath.atan2(myZ - z, myX - x));
+					player.getVehicleBeingUsed().setLocalRotation(
+						new Quaternion().fromAngleAxis(playerDirection - 1.55f,
+								Vector3f.UNIT_Y));
+				}
+
 			}
-			
-			float playerDirection = -(FastMath.atan2(myZ - z, myX - x));
-			
-			if(!player.inVehicle()){
-				player.setLocalRotation(new Quaternion().fromAngleAxis(playerDirection, Vector3f.UNIT_Y));
-			}
-			else{
-				player.getVehicleBeingUsed().setLocalRotation(new Quaternion().fromAngleAxis(playerDirection-1.55f, Vector3f.UNIT_Y));
-				//player.getVehicleBeingUsed().rotateUpTo(new Vector3f());
-			}
-			
+
 			walking = false;
-			
+
+			// if I'm close enough to go on to the next thing...
 			if ((Math.abs(myX - x) < .1f) && (Math.abs(myZ - z) < .1f)) {
 				this.counter++;
-			} else if(!player.inVehicle()){
-	
+
+			} else if (!player.inVehicle()) {
+
 				if ((myX < x) && (Math.abs(myX - x) > .07f)) {
 					walking = true;
-					player.setLocalTranslation(myX + .035f, .1f, myZ);
-	
+					player.setLocalTranslation(myX + .035f, .01f, myZ);
+
 				} else if ((myX > x) && (Math.abs(myX - x) > .07f)) {
 					walking = true;
-					player.setLocalTranslation(myX - .035f, .1f, myZ);
+					player.setLocalTranslation(myX - .035f, .01f, myZ);
 				}
-	
+
 				if ((myZ < z) && Math.abs(myZ - z) > .07f) {
 					walking = true;
-					player.setLocalTranslation(myX, .1f, myZ + .035f);
+					player.setLocalTranslation(myX, .01f, myZ + .035f);
 				} else if ((myZ > z) && Math.abs(myZ - z) > .07f) {
 					walking = true;
-					player.setLocalTranslation(myX, .1f, myZ - .035f);
+					player.setLocalTranslation(myX, .01f, myZ - .035f);
 				}
-			}
-			else{
+			} else {
 				walking = false;
 				if ((myX < x) && (Math.abs(myX - x) > .07f)) {
-					player.getVehicleBeingUsed().setLocalTranslation(myX + .070f, .1f, myZ);
-	
+					player.getVehicleBeingUsed().setLocalTranslation(
+							myX + .070f, .01f, myZ);
+
 				} else if ((myX > x) && (Math.abs(myX - x) > .07f)) {
-					player.getVehicleBeingUsed().setLocalTranslation(myX - .070f, .1f, myZ);
+					player.getVehicleBeingUsed().setLocalTranslation(
+							myX - .070f, .01f, myZ);
 				}
-	
+
 				if ((myZ < z) && Math.abs(myZ - z) > .07f) {
-					player.getVehicleBeingUsed().setLocalTranslation(myX, .1f, myZ + .070f);
+					player.getVehicleBeingUsed().setLocalTranslation(myX, .01f,
+							myZ + .070f);
 				} else if ((myZ > z) && Math.abs(myZ - z) > .07f) {
-					player.getVehicleBeingUsed().setLocalTranslation(myX, .1f, myZ - .070f);
+					player.getVehicleBeingUsed().setLocalTranslation(myX, .01f,
+							myZ - .070f);
 				}
 			}
 		}
-		
-		if(counter >= path.size()){
+
+		if (counter >= path.size()) {
 			deactivate();
 		}
-		if(!walking){
+		if (!walking) {
 			player.stationaryAnim();
-		}
-		else{
+		} else {
 			player.translateForwardAnim();
 		}
 	}
